b195302eba93b1f0596539e7a29adf945e9b9a2a
DamirPages <123557281+DamirPages@users.noreply.github.com>
2023-12-19 22:20:10 +0600

    Add files via upload

--- static/js/blocks.js
+++ static/js/blocks.js
@@ -1,2 +1,83 @@
 import { initHandlersModal} from '../../components/modals/modals';
 initHandlersModal();
+
+  /* SLIDE UP */
+  let slideUp = (target, duration=500) => {
+
+    target.style.transitionProperty = 'height, margin, padding';
+    target.style.transitionDuration = duration + 'ms';
+    target.style.boxSizing = 'border-box';
+    target.style.height = target.offsetHeight + 'px';
+    target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    window.setTimeout( () => {
+          target.style.display = 'none';
+          target.style.removeProperty('height');
+          target.style.removeProperty('padding-top');
+          target.style.removeProperty('padding-bottom');
+          target.style.removeProperty('margin-top');
+          target.style.removeProperty('margin-bottom');
+          target.style.removeProperty('overflow');
+          target.style.removeProperty('transition-duration');
+          target.style.removeProperty('transition-property');
+          //alert("!");
+    }, duration);
+}
+
+/* SLIDE DOWN */
+let slideDown = (target, duration=500) => {
+    target.style.removeProperty('display');
+    let display = window.getComputedStyle(target).display;
+    if (display === 'none') display = 'block';
+    target.style.display = display;
+    let height = target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    target.offsetHeight;
+    target.style.boxSizing = 'border-box';
+    target.style.transitionProperty = "height, margin, padding";
+    target.style.transitionDuration = duration + 'ms';
+    target.style.height = height + 'px';
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+    window.setTimeout( () => {
+      target.style.removeProperty('height');
+      target.style.removeProperty('overflow');
+      target.style.removeProperty('transition-duration');
+      target.style.removeProperty('transition-property');
+    }, duration);
+}
+
+/* TOOGLE */
+var slideToggle = (target, duration = 500) => {
+    if (target.classList.contains('hidden')) {
+        target.classList.remove('hidden');
+        return slideDown(target, duration);
+    } else {
+        target.classList.add('hidden');
+        return slideUp(target, duration);
+    }
+}
+
+const moduleBlocks = document.querySelectorAll('.learnblock__group');
+moduleBlocks.forEach(item => {
+    const button = item.querySelector('.learn-button');
+    const container = item.querySelector('.learnblock__group-moduls');
+    button.addEventListener('click', () => {
+        item.classList.toggle('list-visible');
+        slideToggle(container);
+    });
+    container.style.height = 0;
+    container.classList.add('hidden');
+});
