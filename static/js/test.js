df7fb8e063585fe238889259e6efca4c727d71cb
DamirPages <123557281+DamirPages@users.noreply.github.com>
2023-12-27 02:35:38 +0600

    Add files via upload

--- static/js/test.js
+++ static/js/test.js
@@ -1,5 +1,9 @@
 const testContainers = document.querySelectorAll('.test__container');
 const rounds = document.querySelectorAll('.test__round');
+const wrong = document.querySelector('.result-wrong');
+const pass = document.querySelector('.result-pas');
+const resultElement = document.querySelectorAll('.result-count');
+const testElement = document.querySelector('.test');
 const results = [];
 
 testContainers.forEach((container, index) => {
@@ -15,6 +19,9 @@ testContainers.forEach((container, index) => {
         results.push(showResults(container));
     });
     next.addEventListener('click', ()=>{
+        if(next.classList.contains('get-results')){
+            calcResult();
+        }
         testContainers.forEach(item => item.classList.remove('active'));
         testContainers[index + 1].classList.add('active');
         rounds.forEach(item => item.classList.remove('active'));
@@ -46,5 +53,24 @@ function showResults(container){
     }
     showButton.classList.add('hidden');
     nextQuest.classList.remove('hidden');
+    console.log(results);
     return !error;
+}
+function calcResult(){
+    const trueResults = results.reduce((acc, el) => {
+        if(el){
+            acc++;
+        }
+        return acc;
+    }, 0);
+    const percent = Math.round(trueResults * 100 / results.length);
+    resultElement.forEach(item => {
+        item.textContent = percent + '%';
+        testElement.classList.add('hidden-block');
+        if(percent > 79){
+            pass.classList.remove('result-pas');
+        }else{
+            wrong.classList.remove('result-wrong');
+        }
+    })
 }
